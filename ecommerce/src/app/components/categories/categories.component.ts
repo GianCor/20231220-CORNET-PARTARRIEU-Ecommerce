c2668803b4e75c419650c608086d6087fcfa38ce
unknown <apartarrieu@petersen.corp>
2023-12-20 12:30:46 -0300

    ruteo dinamico

--- ecommerce/src/app/components/categories/categories.component.ts
+++ ecommerce/src/app/components/categories/categories.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoriesService } from 'src/app/services/categories.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.css']
 })
-export class CategoriesComponent {
-
+export class CategoriesComponent implements OnInit {
+  id: any;
+  arrProducts:any = [];
+  
+  
+  
+  constructor(public categoryService: CategoriesService, private route: ActivatedRoute){}
+  
+  
+  ngOnInit(): void {
+    
+    console.log('hola');
+    
+    this.id = this.route.snapshot.paramMap.get('category');
+    console.log(this.id);
+      
+    this.categoryService.getProductByCategory(this.id).subscribe((e)=>{
+      this.arrProducts = e;
+      console.log(e);
+      
+    })    
+  }
 }
